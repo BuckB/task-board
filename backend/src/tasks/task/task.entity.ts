54373edca88f06efc91cb13e019abff7aecfb429
Bruno Buck <b.brigagao@gmail.com>
2026-03-12 16:59:33 -0300

    Introduce persistent Task service and controller
    - Create Task entity with TypeORM decorators.
    - Implement TasksService using Repository pattern for CRUD.
    - Define TasksController for RESTful interaction.
    - Move from in-memory logic to database persistence.
    - Complete infrastructure smoke test with Podman and Alpine Postgres.

---
+++ backend/src/tasks/task/task.entity.ts
@@ -0,0 +1,28 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { TaskStatus } from "./task-status.enum";
+
+@Entity()
+export class Task {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    title: string;
+
+    @Column()
+    description: string;
+
+    @Column({
+        type: 'enum',
+        enum: TaskStatus,
+        default: TaskStatus.BACKLOG
+    })
+    status: TaskStatus;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+}
